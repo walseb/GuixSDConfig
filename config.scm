7fc15c07b5fd668b71433c4c4a6f1e6644fdef7b
Sebastian Walinder <s.walinder@gmail.com>
2018-10-07 00:33:28 +0200

    Initial commit

---
+++ config.scm
@@ -0,0 +1,109 @@
+;; This is an operating system configuration template
+;; for a "desktop" setup with GNOME and Xfce where the
+;; root partition is encrypted with LUKS.
+
+(use-modules (gnu) (gnu system nss) (gnu packages admin) (gnu packages multiprecision) (gnu packages mpd) (gnu packages gnupg) (gnu packages pdf) (gnu packages compression) (gnu packages xdisorg) (gnu 
+packages fonts) (gnu packages xorg) (gnu packages imagemagick) (gnu packages code))
+
+(use-service-modules networking ssh)
+
+(use-service-modules desktop)
+
+(use-package-modules 
+                     certs
+                     version-control
+                     xorg
+                     emacs
+                     aspell
+                     wget
+                     ssh
+                     pulseaudio
+                     )
+
+(operating-system
+ (host-name "guix-VM")
+ (timezone "Europe/Stockholm")
+ (locale "en_US.utf8")
+
+ ;; Use UEFI
+ (bootloader (bootloader-configuration
+              (bootloader grub-efi-bootloader)
+              (target "/boot/efi")))
+
+ ;; Use BIOS
+ ;;(bootloader (bootloader-configuration
+ ;;              (bootloader grub-bootloader)
+ ;;              ;; VM disk
+ ;;              (target "/dev/vda")))
+
+ ;; VM support
+ (initrd-modules (append (list "virtio_blk" "virtio_pci" "shpchp")
+                         %base-initrd-modules))
+
+ ;; Mount disk
+ (file-systems (cons (file-system
+                      (device (file-system-label "my-root"))
+                      (mount-point "/")
+                      (type "ext4"))
+                     %base-file-systems))
+
+ ;; Setup user
+ (users (cons (user-account
+               (name "admin")
+               (comment "")
+               (group "users")
+               (supplementary-groups '("wheel" "netdev"
+                                       "audio" "video"))
+               (home-directory "/home/admin"))
+              %base-user-accounts))
+
+ ;; This is where we specify system-wide packages.
+ (packages (cons* nss-certs         ;for HTTPS access
+;;		  xorg-server
+;;		  xinit
+		  xrandr
+                  setxkbmap
+                  pulseaudio
+                  pavucontrol
+                  emacs
+		  openssh
+		  imagemagick
+		  the-silver-searcher
+		  ;;mono
+		 ;; sensors
+		  poppler
+		  gnupg
+		  pinentry
+		  redshift
+		  mpd
+		  mpc
+;;		  amixer
+		  atool
+		  htop
+                  git
+                  wget
+                  aspell
+                  aspell
+                  aspell-dict-en
+                  aspell-dict-sv
+                  unzip
+                  rxvt-unicode
+		  ;; Fonts
+                  font-inconsolata
+                  font-gnu-freefont-ttf
+                  %base-packages))
+
+ ;; Add GNOME and/or Xfce---we can choose at the log-in
+ ;; screen with F1.  Use the "desktop" services, which
+ ;; include the X11 log-in service, networking with
+ ;; NetworkManager, and more.
+ ;; (services (cons* (gnome-desktop-service)
+ ;;                  (xfce-desktop-service)
+ ;;                  %desktop-services))
+ (services (cons*
+;;            (dhcp-client-service)
+            %desktop-services))
+
+ ;; Allow resolution of '.local' host names with mDNS.
+ (name-service-switch %mdns-host-lookup-nss))
+
