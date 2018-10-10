a5c2a9b0a2ccaf787c5dd3eafa15586241ee968e
Sebastian Walinder <s.walinder@gmail.com>
2018-10-10 21:23:31 +0200

    Fixed some things

--- config.scm
+++ config.scm
@@ -1,17 +1,19 @@
-(use-modules (gnu) (gnu system nss) (gnu packages disk) (gnu packages admin) (gnu packages linux) (gnu packages glib) (gnu packages gl)(gnu packages libunwind)(gnu packages nettle) (gnu packages multiprecision) (gnu packages mpd) (gnu packages gnupg) (gnu packages pdf) (gnu packages compression) (gnu packages xdisorg) (gnu packages fonts) (gnu packages xorg) (gnu packages imagemagick) (gnu packages code))
+(use-modules (gnu) (gnu system nss) (gnu packages display-managers) (gnu packages bash) (rnrs lists) (gnu services xorg) (gnu packages disk) (gnu packages admin) (gnu packages linux) (gnu packages glib) (gnu packages gl)(gnu packages libunwind)(gnu packages nettle) (gnu packages multiprecision) (gnu packages mpd) (gnu packages gnupg) (gnu packages pdf) (gnu packages compression) (gnu packages xdisorg) (gnu packages fonts) (gnu packages xorg) (gnu packages imagemagick) (gnu packages code))
+
+(use-service-modules desktop)
 
 (use-service-modules networking ssh)
 
 (use-package-modules 
-                     certs
-                     version-control
-                     xorg
-                     emacs
-                     aspell
-                     wget
-                     ssh
-                     pulseaudio
-                     )
+ certs
+ version-control
+ xorg
+ emacs
+ aspell
+ wget
+ ssh
+ pulseaudio
+ )
 
 (operating-system
  (host-name "guix-VM")
@@ -21,21 +23,15 @@
  
  ;; Mount disk
  (file-systems (cons (file-system
-                      (device (file-system-label "my-root"))
-                      (mount-point "/")
-                      (type "ext4"))
+		      (device (file-system-label "my-root"))
+		      (mount-point "/")
+		      (type "ext4"))
 		     (cons (file-system
-			    (device "/dev/vda1/")
+			    (device "/dev/vda1")
 			    (mount-point "/boot/efi/")
 			    (type "vfat"))
 			   %base-file-systems)))
  
- 
- ;; (file-systems (file-system
- ;; (device "/dev/vda1/")
- ;; (mount-point "/boot/efi/")
- ;; (type "vfat")))
-
  ;; Use UEFI
  (bootloader (bootloader-configuration
               (bootloader grub-efi-bootloader)
@@ -47,47 +43,47 @@
  ;;              ;; VM disk
  ;;              (target "/dev/sda")))
 
- ;; VM support
+ ;; VM support - "shpchp" causes problems
  (initrd-modules (append (list "virtio_blk" "virtio_pci")
                          %base-initrd-modules))
 
  ;; Setup user
  (users (cons (user-account
-               (name "admin")
-               (comment "")
-               (group "users")
-               (supplementary-groups '("wheel" "netdev"
-                                       "audio" "video"))
-               (home-directory "/home/admin"))
+	       (name "admin")
+	       (comment "")
+	       (group "users")
+	       (supplementary-groups '("wheel" "netdev" "users" "audio" "video"))
+	       (home-directory "/home/admin"))
               %base-user-accounts))
 
  ;; This is where we specify system-wide packages.
  (packages (cons* nss-certs         ;for HTTPS access
 		  ;; Xorg
+		  bash
 		  xorg-server
-		  xinit
+		  ;;xinit
 		  xrandr
-		  xf86-input-evdev
                   setxkbmap
 		  ;; Dependencies
-		  dbus
-		  libdrm
-		  libepoxy
-		  ;;libgl
-		  libpciaccess
-		  libunwind
-		  libxfont
-		  libxshmfence
-		  nettle
-		  pixman
+		  ;; dbus
+		  ;; libdrm
+		  ;; libepoxy
+		  ;; libpciaccess
+		  ;; libunwind
+		  ;; libxfont
+		  ;; libxshmfence
+		  ;; nettle
+		  ;; pixman
 		  xf86-input-libinput
 		  ;; Xinit dependencies
-		  inetutils
-		  libx11
-		  xauth
-		  xmodmap
-		  xrdb
+		  ;; inetutils
+		  ;; libx11
+		  ;; xauth
+		  ;; xmodmap
+		  ;; xrdb
 
+		  icecat
+		  slim
                   pulseaudio
                   pavucontrol
                   emacs
@@ -127,9 +123,13 @@
  ;; (services (cons* (gnome-desktop-service)
  ;;                  (xfce-desktop-service)
  ;;                  %desktop-services))
- ;; (services (cons*
- ;;            (dhcp-client-service)
- ;;          %desktop-services))
-
+ 
+ ;;(dhcp-client-service)
+ (services 
+  (remove (lambda (service)
+	    (eq? (service-kind service) screen-locker-service-type))
+	  %desktop-services))
+ ;;(remove-elem %desktop-services (screen-locker-service slock) (screen-locker-service xlockmore "xlock"))))
+ 
  ;; Allow resolution of '.local' host names with mDNS.
  (name-service-switch %mdns-host-lookup-nss))
